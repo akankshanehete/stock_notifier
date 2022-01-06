cf5d10faf5cfc8f7fe021d769136185f552076a6
akankshanehete <nehetea@mcmaster.ca>
2022-01-06 14:57:09 -0500

    added text functionality
    Signed-off-by: akankshanehete <nehetea@mcmaster.ca>

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ COMPANY_NAME = "Tesla Inc"
 
 # getting data from alphavantage stock API
 stock_parameters = {
-    'apikey': '///////////////',
+    'apikey': '///////////',
     'function': 'TIME_SERIES_INTRADAY',
     'symbol': STOCK,
     'interval': '60min'
@@ -26,43 +26,43 @@ dby_cl = float(stock_data[str(day_before_yesterday) + " 20:00:00"]['4. close'])
 
 # getting data from news API
 news_parameters = {
-    'apiKey': '///////////////',
-    'q' : COMPANY_NAME,
+    'apiKey': '//////////////',
+    'q': COMPANY_NAME,
     'from': str(yesterday),
     'to': str(today)
 }
 
-news_data = requests.get(url='https://newsapi.org/v2/everything', params=news_parameters).json()
-print(news_data)
-
-
 # function checks the percent change between 2 values
-def check_percent_change(initial_value, final_value):
+def check_percent_change (initial_value, final_value):
     percent_chg = ((final_value - initial_value)/initial_value) * 100
     return int(percent_chg)
 
-print(y_cl, dby_cl)
-print(check_percent_change(dby_cl, y_cl))
+p_diff = check_percent_change(dby_cl, y_cl)
+
+# twilio account information
+account_sid = "ACfac3be954ae620d6aa6dfadc5b4d9e7c"
+auth_token = "/////////////////"
 
 # when stock price increases/decreases by 5% or more b/w yesterday and the day before yesterday,
-# news articles are fetched for that company
-if check_percent_change(dby_cl,y_cl) >= 5 and check_percent_change(dby_cl,y_cl) <= -5:
-    print("Get News!")
+# news articles are fetched for that company and text message is sent
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number.
-
-
-#Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+if p_diff >= 5 and p_diff <= -5:
+    news_data = requests.get(url='https://newsapi.org/v2/everything', params=news_parameters).json()['articles'][0:3]
+    article1 = (news_data[0]['title'], news_data[0]['description'][0:100]+'...')
+    article2 = (news_data[1]['title'], news_data[1]['description'][0:100]+'...')
+    article3 = (news_data[2]['title'], news_data[2]['description'][0:100]+'...')
+    if p_diff > 0:
+        text_diff = '🔺' + str(p_diff)+'%'
+    else:
+        text_diff = '🔻' + str(p_diff)[1:2] + '%'
+    message = "TSLA: " + text_diff + "\n Headline: " + article1[0]+"\n Brief: " + article1[1]
+    # creating a client from the twilio API
+    client = Client(account_sid, auth_token)
+    # sending a rain message if it is going to rain that day
+    message = client.messages.create(
+        body= message,
+        from_='+13233104492',
+        to='+16477676905'
+    )
+else:
+    print("not a significant stock change")
